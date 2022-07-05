f6ca30c6d34bc857de1ff6a4ec1205e36cfab6c4
sodji98 <107852003+sodji98@users.noreply.github.com>
2022-07-05 08:56:01 +0200

    Update f.c

--- f.c
+++ f.c
@@ -1,12 +1,7 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
-#define A _a
-
-int main()
+int f()
 {
-  int a;
-  int A;
-  int a_;
-  printf("hello world\n");
+  
 }
