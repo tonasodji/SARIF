3a618e44ff14c5f0b0670c4a2d04578d043a6da6
sodji98 <107852003+sodji98@users.noreply.github.com>
2022-07-14 13:20:52 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 touch makefile
 echo "file.o: f.c" > makefile
-echo -e "\tgcc -c --all-warnings --extra-warnings -W f.c" >> makefile
+echo -e "\tgcc -c --all-warnings --extra-warnings -W src/*.c" >> makefile
 pvs-studio-analyzer trace -- make -j8
 pvs-studio-analyzer analyze -j8 -l PVS-Studio.lic -o PVS-Studio.log
 plog-converter PVS-Studio.log -t sarif -o results.sarif
