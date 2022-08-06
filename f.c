c60a721d735ad6d42e3524c20bcf75bd7546ee8e
sodji98 <107852003+sodji98@users.noreply.github.com>
2022-08-06 20:46:03 +0200

    Update f.c

--- f.c
+++ f.c
@@ -1,7 +1,18 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
-int f()
+int a(int a, int b, int c)
+  
+int b(int a, int b, int c)
+  
+int c(int a, int b, int c)
+  
+  
+int
+main()
 {
   
+  int a, b, c;
+  
+  return 0;
 }
